f1af956a83ee9cc377e71a2e1cf0d34a913d2ba2
Yasmine-Mustafa <75459081+Yasmine-Mustafa@users.noreply.github.com>
2020-12-22 11:58:29 -1000

    Moved the files
    I added the files to the FTC Team code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/halfFieldRedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/halfFieldRedAuto.java
@@ -1,13 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
 public class halfFieldRedAuto {
 }
-    robotDeclarations         robot   = new robotDeclarations();
+    robotDeclarations robot   = new robotDeclarations();
 
 
 
-    static final double COUNTS_PER_MOTOR_REV    = 1440 ;
+static final double COUNTS_PER_MOTOR_REV    = 1440 ;
 static final double DRIVE_GEAR_REDUCTION    = 2.0 ;
 static final double WHEEL_DIAMETER_INCHES   = 4.0 ;
 static final double COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
                                               (WHEEL_DIAMETER_INCHES * 3.1415);
 static final double  DRIVE_SPEED            = 0.6;
 static final double  TURN_SPEED             = 0.5;
+
+@Override public void runOpMode() {}
