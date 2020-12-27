50a9d8805d493e4d703d2027af97ec35e5e41e65
Yasmine-Mustafa <75459081+Yasmine-Mustafa@users.noreply.github.com>
2020-12-27 10:35:40 -1000

    Update halfFieldRedAuto.java
    Added more code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/halfFieldRedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/halfFieldRedAuto.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
-public class halfFieldRedAuto {
+public class halfFieldRedAuto extends TeleopDriveCode {
 }
     robotDeclarations robot   = new robotDeclarations();
 
@@ -14,4 +14,39 @@ static final double COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR
 static final double  DRIVE_SPEED            = 0.6;
 static final double  TURN_SPEED             = 0.5;
 
-@Override public void runOpMode() {}
+@Override public void runOpMode() {
+
+robot.init(hardwareMap);
+
+
+telemetry.addData("Status", "Resetting Encoders");   //
+telemetry.update();
+
+robot.leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+robot.rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+robot.leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+robot.rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+robot.leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+robot.rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+robot.leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+robot.rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+telemetry.addData("Path0",  "Starting at %7d :%7d",
+        robot.leftFrontDrive.getCurrentPosition(),
+        robot.rightFrontDrive.getCurrentPosition());
+        robot.leftBackDrive.getCurrentPosition(),
+        robot.rightBackDrive.getCurrentPosition();
+telemetry.update();
+
+//wait for game to start (press PLAY)
+waitForStart();
+
+    encoderDrive(DRIVE_SPEED,  44,  44, 5.0);  // S1: Forward 44 Inches with 5 Sec timeout
+    encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+    encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+
+
+
+
+
