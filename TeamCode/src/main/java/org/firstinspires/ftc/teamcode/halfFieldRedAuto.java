244d1e8200c9bdbe94a33487d140731d170a9507
Yasmine-Mustafa <75459081+Yasmine-Mustafa@users.noreply.github.com>
2021-01-19 15:45:34 -1000

    Update halfFieldRedAuto.java
    Added the movement of the robot but still needs to shoot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/halfFieldRedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/halfFieldRedAuto.java
@@ -20,8 +20,16 @@ public class halfFieldRedAuto extends robotDeclarations {
                 robot.rightFrontDrive.getCurrentPosition());
         telemetry.update();
 
+        //Wait for game to start
+        waitForStart();
 
-
-            }
-        }
+        //movements
+        robot.encoderDrive(1.0, 44.0, 44.0, 5.0); //Forward 44 inches with a 5 second timeout
+        robot.turnDrive( 0.5, 45.0, 5.0 ); // Turn right 45 degree
+        robot.encoderDrive(1.0, 22.6, 22.6, 5.0); //Forward 22.6 inches
+        robot.turnDrive(1.0, -45.0, 5.0); // Turn right 45 degrees
+        //you still need to shoot-- make a pivot and shooter motor
+        robot.encoderDrive();
+    }
+}
 
